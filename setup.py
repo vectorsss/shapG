7aeaab076f90c55bfebe2f6c41854a35ab3fa8e9
Chi Zhao <dandanv5@hotmail.com>
2025-03-20 02:36:02 +0300

    [chore]: change setup.py, README.md

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="shapG",
     version="0.13.2-dev",
@@ -22,13 +25,18 @@ setup(
     author="Chi Zhao",
     author_email="dandanv5@hotmail.com",
     description="A library to compute Shapley values in graphs.",
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url="https://github.com/vectorsss/shapG",
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Information Analysis",
     ],
     python_requires='>=3.6',
 )
